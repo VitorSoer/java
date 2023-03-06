3faea042e2c3d01e835471263851415746404e8a
vitorsoer <contato.soer@gmail.com>
2023-03-06 20:14:06 -0300

    Repo config

---
+++ course_01/basics/output.java
@@ -0,0 +1,16 @@
+public class output {
+
+	public static void main(String[] args) {
+		String username = "Jhon Doe";
+		int age = 22;
+		double salary = 22.55000;
+		
+		// %f = float | %d = int | %s = string | %n = line break
+		System.out.printf(
+			"User: %s | Age: %d | Salary: %.3f",
+			username, age, salary
+		);
+
+	}
+
+}
